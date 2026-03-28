250c017de1e8ba193210dcb3dc20c920f63c7a25
danielkapera <djuanrique@gmail.com>
2026-03-28 09:20:05 -0500

    otro mas

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -12,7 +12,14 @@ class DetailScreen extends StatelessWidget {
     return Scaffold
     (
       body: CustomScrollView(
-        slivers: [_CustomAppBar()],
+        slivers: [
+          _CustomAppBar(),
+          SliverList(delegate: SliverChildListDelegate(
+            [ _PosterAndTitle(), 
+             _Overview()
+            ]
+          ))
+        ],
       )
     );
   }
@@ -44,5 +51,65 @@ class _CustomAppBar extends StatelessWidget {
           )
         )
       );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: AssetImage('assets/no-image.jpg'),
+              height: 150,
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Movie Title',
+                style: textTheme.headlineSmall,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  SizedBox(width: 5),
+                  Text('Movie.voteAverage', style: textTheme.bodyMedium)
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Overview of the movie goes here...',
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.labelMedium,
+      ),
+      
+    );
   }
 }
---
+++ lib/widgets/casting_cards.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CastingCards extends StatelessWidget {
+  const CastingCards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
